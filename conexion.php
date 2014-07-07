ff5c0e053c59aab4dfb679835f016237231795f7
Ailin Belen <ailin.silva@gmail.com>
2014-07-06 21:11:11 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
  
 ///Conectamos a la DB
  session_start();
-$conexion = mysql_connect("localhost","root","lalina");
+$conexion = mysql_connect("localhost","root","");
 mysql_select_db("login", $conexion);
 
 if (!$conexion)
